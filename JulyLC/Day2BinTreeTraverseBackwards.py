648eaa5df5231aeebca07dfd4c4e525f41e96382
Watson Fu <watsonfu18@gmail.com>
2020-07-02 10:57:50 -0500

    2deque

--- JulyLC/Day2BinTreeTraverseBackwards.py
+++ JulyLC/Day2BinTreeTraverseBackwards.py
@@ -4,9 +4,10 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+import collections
 class Solution:
     def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
-        def levelTraverse(nodes, res=[]):
+        def levelTraverse(nodes, res=collections.deque([])):
             new_nodes = []
             curr_values = []
             for node in nodes:
@@ -15,9 +16,9 @@ class Solution:
                 if node.right:
                     new_nodes.append(node.right)
                 curr_values.append(node.val)
-            res.append(curr_values)
+            res.appendleft(curr_values)
             if not new_nodes:
-                return res[::-1]
+                return res
             return levelTraverse(new_nodes, res)
 
         if not root:
