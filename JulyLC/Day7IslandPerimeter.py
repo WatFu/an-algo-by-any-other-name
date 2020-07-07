2ab95e4ca443b161870177155b1f21c7eaaef6f7
Watson Fu <watsonfu18@gmail.com>
2020-07-07 11:07:37 -0500

    Update Day7IslandPerimeter.py

--- JulyLC/Day7IslandPerimeter.py
+++ JulyLC/Day7IslandPerimeter.py
@@ -8,7 +8,8 @@ class Solution:
                 return 0
             return 1
         
-        def bfs(curr_square, count):
+        def bfs(curr_square):
+            count = 0
             visited[(curr_square[0],curr_square[1])] = 1
             
             up = (curr_square[0] - 1, curr_square[1])
@@ -16,26 +17,25 @@ class Solution:
             right = (curr_square[0], curr_square[1] + 1)
             left = (curr_square[0], curr_square[1] - 1)
             
-            curr_count = count
             
             if is_valid(up) and grid[up[0]][up[1]]:
                 if not up in visited:
-                    count += bfs(up, curr_count)
+                    count += bfs(up)
             else:
                 count += 1
             if is_valid(down) and grid[down[0]][down[1]]:
                 if not down in visited:
-                    count += bfs(down, curr_count)
+                    count += bfs(down)
             else:
                 count += 1
             if is_valid(right) and grid[right[0]][right[1]]:
                 if not right in visited:
-                    count += bfs(right, curr_count)
+                    count += bfs(right)
             else:
                 count += 1
             if is_valid(left) and grid[left[0]][left[1]]:
                 if not left in visited:
-                    count += bfs(left, curr_count)
+                    count += bfs(left)
             else:
                 count += 1
             return count
@@ -53,4 +53,4 @@ class Solution:
         if start_square == (-1, -1):
             return 0
         
-        return bfs(start_square, 0)
+        return bfs(start_square)
