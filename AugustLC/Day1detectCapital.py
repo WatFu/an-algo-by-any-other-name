c795125c1c16dd31e2b135096a376803a6ed647c
Watson Fu <watsonfu18@gmail.com>
2020-08-01 09:31:40 -0500

    Update Day1detectCapital.py

--- AugustLC/Day1detectCapital.py
+++ AugustLC/Day1detectCapital.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if len(word) == 1 or len(word) == 0:
+            return True
+        
+        if ord(word[0]) >= 97:
+            for i in range(1,len(word)):
+                if (ord(word[i])) < 97:
+                    return False
+            return True
+        
+        if ord(word[1]) >= 97:
+            for i in range(2,len(word)):
+                if (ord(word[i])) < 97:
+                    return False
+            return True
+        
+        for i in range(2,len(word)):
+            if (ord(word[i])) >= 97:
+                return False
+        return True
