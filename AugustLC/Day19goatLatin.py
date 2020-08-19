0f770d3e4ab3890b5d71702d09bfe2bf1eede3e5
Watson Fu <watsonfu18@gmail.com>
2020-08-19 08:19:11 -0500

    Create Day19goatLatin.py

---
+++ AugustLC/Day19goatLatin.py
@@ -0,0 +1,4 @@
+class Solution:
+    def toGoatLatin(self, S: str) -> str:
+        vowels = {'a','e','i','o','u','A','E','I','O','U'}
+        return ' '.join([x + 'ma'+ 'a' * (i + 1) if x[0] in vowels else x[1:] + x[0] + 'ma'+ 'a' * (i + 1) for (i, x) in enumerate(S.split())])
