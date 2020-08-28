1c754cd5bef4f8426a040aa73447f443d6e45561
Watson Fu <watsonfu18@gmail.com>
2020-08-28 09:54:22 -0500

    Create Day28rand7.py

---
+++ AugustLC/Day28rand7.py
@@ -0,0 +1,11 @@
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+import random
+
+class Solution:
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        return random.randint(1,10)
