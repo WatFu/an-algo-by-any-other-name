297c4b76f171b337526236382e3d831200c8b71d
Watson Fu <watsonfu18@gmail.com>
2020-08-31 12:03:55 -0500

    Create Day31deleteNodeInBST.py

---
+++ AugustLC/Day31deleteNodeInBST.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return root
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.right:
+                return root.left
+            elif not root.left:
+                return root.right
+            else:
+                node = root.left
+                val = node.val
+                while node.right:
+                    node = node.right
+                    val = node.val
+                root.val = val
+                root.left = self.deleteNode(root.left, val)
+        return root
