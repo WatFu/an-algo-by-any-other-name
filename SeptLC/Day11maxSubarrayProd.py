c94465ee8232a4b1322c718e5d04bbc19b53fd4c
Watson Fu <watsonfu18@gmail.com>
2020-09-11 13:00:40 -0500

    Update Day11maxSubarrayProd.py

--- SeptLC/Day11maxSubarrayProd.py
+++ SeptLC/Day11maxSubarrayProd.py
@@ -1,5 +1,42 @@
+# class Solution:
+#     def maxProduct(self, nums: List[int]) -> int:
+#         res = -math.inf
+#         pos = -math.inf
+#         neg = -math.inf
+        
+#         for num in nums:
+#             if num > 0:
+#                 if pos == -math.inf or pos == 0:
+#                     pos = num
+#                 else:
+#                     pos *= num
+#                 if neg == -math.inf or neg == 0:
+#                     neg = num
+#                 else:
+#                     neg *= num
+#             elif num < 0:
+#                 if neg == -math.inf or neg == 0:
+#                     neg = num
+#                 else:
+#                     neg *= num
+#                 res = max(res, neg, pos)
+#                 pos = -math.inf
+            
+#             else:
+#                 res = max(res, neg, pos)
+#                 pos = 0
+#                 neg = 0
+        
+        
+                
+        
+#         return max(res, pos, neg)
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        return max(self.helper(nums), self.helper(nums[::-1]))
+        
+    def helper(self, nums):
         res = -math.inf
         pos = -math.inf
         neg = -math.inf
@@ -14,6 +51,7 @@ class Solution:
                     neg = num
                 else:
                     neg *= num
+                res = max(res, neg, pos)
             elif num < 0:
                 if neg == -math.inf or neg == 0:
                     neg = num
@@ -26,8 +64,5 @@ class Solution:
                 res = max(res, neg, pos)
                 pos = 0
                 neg = 0
-        
-        
-                
         
         return max(res, pos, neg)
